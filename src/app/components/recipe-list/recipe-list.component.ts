2aaba9e0ddc0824c071cc9ca0d5a78b05c6ecdf1
SuperBrzezinski <michal.a.brzezinski@gmail.com>
2022-03-31 00:09:33 +0200

    Added chips for sorting in Recipe-List component
    For now user can add chips but cant delete them.
    
    Also small changes in other files, but not important.

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface SortType {
+  name: string;
+  category: 'lexOrder' | 'rating';
+  inUse: boolean;
+}
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  public sorts: SortType[] = [
+    { name: 'A-Z', category: 'lexOrder', inUse: false },
+    { name: 'Z-A', category: 'lexOrder', inUse: false },
+    { name: '5-1', category: 'rating', inUse: false },
+    { name: '1-5', category: 'rating', inUse: false },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addSort(name: string, category: 'lexOrder' | 'rating') {
+    // if (this.sorts.includes({name: string category: category })) {
+    // }
+    this.sorts.forEach((entry) => {
+      if (entry.category === category) {
+        entry.inUse = false;
+      }
+    });
+    let index = this.sorts.findIndex((entry) => entry.name === name);
+    this.sorts[index].inUse = !this.sorts[index].inUse;
   }
-
 }
