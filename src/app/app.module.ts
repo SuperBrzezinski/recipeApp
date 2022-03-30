ebdce5785f963a2d59de81b3e706ec419c8068ff
SuperBrzezinski <michal.a.brzezinski@gmail.com>
2022-03-30 22:41:52 +0200

    Added two components: navbar and recipe-list
    Two new components was added in their initial form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
