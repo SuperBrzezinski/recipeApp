1027fe69be5130d6c38c0b7827a54abd206d21a2
SuperBrzezinski <michal.a.brzezinski@gmail.com>
2022-04-04 18:36:50 +0200

    Bug fix: Details doesn't show recipe after changing tab
    In CardToDetailsCommunicatorService and FetchRecipesService Subjects
    were changed to ReplaySubjects, so RecipeDetails always get some value.

--- src/app/services/card-to-details-communicator.service.ts
+++ src/app/services/card-to-details-communicator.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, ReplaySubject } from 'rxjs';
 import { Recipe } from '../interfaces/recipe';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CardToDetailsCommunicatorService {
-  private $recipeChoiceListener = new Subject<Recipe>();
+  private $recipeChoiceListener = new ReplaySubject<Recipe>(1);
 
   constructor() {}
 
--- src/app/services/fetch-recipes.service.ts
+++ src/app/services/fetch-recipes.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../interfaces/recipe';
-import { Subject } from 'rxjs';
+import { Subject, ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FetchRecipesService {
   private dbUrl = 'http://localhost:3000';
-  private $recipes = new Subject<Recipe[]>();
+  private $recipes = new ReplaySubject<Recipe[]>(1);
 
   constructor(private http: HttpClient) {
     this.getRecipes();
